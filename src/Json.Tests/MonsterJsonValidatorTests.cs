8aea619421a2ebf943cf91886b49315bef29e1e9
Dennis Jakupovic <dennis.jakupovic@wearetriple.com>
2022-04-28 20:41:07 +0200

    Add JsonSchemaValidation

---
+++ src/Json.Tests/MonsterJsonValidatorTests.cs
@@ -0,0 +1,63 @@
+﻿using Json.Models;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Json.Tests;
+
+public class MonsterJsonValidatorTests
+{
+    private readonly MonsterJsonValidator _subject;
+    private readonly JSchema _monsterSchema;
+
+    public MonsterJsonValidatorTests()
+    {
+        JSchemaGenerator generator = new();
+        _monsterSchema = generator.Generate(typeof(Monster));
+
+        _subject = new MonsterJsonValidator(_monsterSchema);
+    }
+
+    [Test]
+    public void WhenUsingDefaultValidator_ItShouldReturnAppropriately()
+    {
+        // Arrange
+        JObject monster = JObject.Parse(@"{
+            'Name': 'Monster',
+            'Legs': 4,
+            'Type': 'Wraith',
+            'Code': '123',
+            'Birthday': '2018-03-19T00:00:00.000Z'
+        }");
+
+        IList<string> errorMessages;
+
+        // Act
+        bool isValid = monster.IsValid(_monsterSchema, out errorMessages);
+
+        // Assert
+        Assert.IsTrue(isValid);
+        Assert.IsEmpty(errorMessages);
+    }
+
+    [Test]
+    public void WhenUsingCustomValidator_ItShouldReturnAppropriately()
+    {
+        // Arrange
+        JObject monster = JObject.Parse(@"{
+            'Legs': '4',
+            'Type': 'Cow', /* Does not yet get validated with the Attribute! */
+            'Code': '1234',
+            'Birthday': '2018-03-19'
+        }");
+
+        // Act
+        var validationResult = _subject.ValidateMonsterData(monster);
+
+        // Assert
+        Assert.NotNull(validationResult);
+        Assert.AreEqual(validationResult!.Count, 3);
+    }
+}
---
+++ src/Json/MonsterJsonValidator.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Json;
+
+public class MonsterJsonValidator
+{
+    private readonly JSchema _monsterValidationSchema;
+
+    public MonsterJsonValidator(JSchema profileValidationSchema)
+    {
+        _monsterValidationSchema = profileValidationSchema;
+    }
+
+    public IList<ValidationResult>? ValidateMonsterData(JObject monsterData)
+    {
+        var monsterObject = JObject.FromObject(monsterData);
+
+        if (!monsterObject.IsValid(_monsterValidationSchema, out IList<string> errorMessages))
+        {
+            var validationResult = new List<ValidationResult>();
+            errorMessages.ToList().ForEach(errorMessage => validationResult.Add(new ValidationResult(errorMessage)));
+
+            return validationResult;
+        }
+
+        return null;
+    }
+}
